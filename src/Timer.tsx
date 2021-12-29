e9105adaa915b17e8a5fe5f58575360ee390dfbd
geraud.henrion <geraud.henrion@nartex.fr>
2021-12-29 12:26:28 +0100

    refactor timer

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,45 +1,49 @@
 import { useCycleApp, useStreamify } from "./lib";
-import xs, { MemoryStream, Stream } from "xstream";
-import { AppSinks, AppSources } from "./App";
+import xs, { Stream } from "xstream";
+import { AppSources } from "./App";
 import { WithRequest } from "./RequestMiddleware";
 import { Response } from "@cycle/http";
 
 type Props = { delay: number };
 
-export function Timer(props: Props) {
-  const { delay } = props;
+function useTimer(delay: number) {
   const delay$ = useStreamify(delay);
+  const { data } = useCycleApp(() => {
+    const timer$ = delay$
+      .map((delay) => xs.periodic(delay))
+      .flatten()
+      .fold((prev) => prev + 1, 0);
+
+    return [timer$, { log: delay$ }];
+  }, []);
+
+  return data;
+}
+
+function useRequest(url: string) {
+  const url$ = useStreamify(url);
 
   const { data } = useCycleApp(
     function main(sources: AppSources & WithRequest) {
       const { request } = sources;
 
-      const response$: Stream<Response | null> = delay$
-        .debug("delay$")
-        .map((delay) =>
-          (request({ url: "/lol" + delay }) as any).startWith(null)
-        )
+      const response$: Stream<Response | null> = url$
+        .map((url) => (request({ url }) as any).startWith(null))
         .flatten();
 
-      const timer$ = delay$
-        .map((delay) => xs.periodic(delay))
-        .flatten()
-        .fold((prev) => prev + 1, 0);
-
-      const sinks: AppSinks = {
-        log: delay$,
-      };
-
-      return [
-        xs.combine(response$, timer$).map(([response, timer]) => {
-          return { response, timer };
-        }),
-        sinks,
-      ];
+      return [response$];
     },
-    [delay$]
+    [url$]
   );
-  const { timer, response } = data ?? {};
+
+  return data;
+}
+
+export function Timer(props: Props) {
+  const { delay } = props;
+
+  const response = useRequest(`/lol/${delay}`);
+  const timer = useTimer(delay);
 
   return (
     <>
